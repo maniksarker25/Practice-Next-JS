16963e93b225c2275925a9c932bec47543253df9
Manik <tonysins43@gmail.com>
2023-08-06 14:46:40 +0600

    learn nesting routin and metadata

---
+++ src/app/dashboard/manage-product/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageProductPage = () => {
+    return (
+        <div>
+            ManageProductPage
+        </div>
+    );
+};
+
+export default ManageProductPage;
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const DashboardPage = () => {
+    return (
+        <div>
+            Dashboard Page
+        </div>
+    );
+};
+
+export default DashboardPage;
